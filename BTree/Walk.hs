f3dc31259163221bbb82205ed0b13de77bb3435d
Ben Gamari <bgamari.foss@gmail.com>
2013-08-26 09:18:30 -0400

    Walk: Add walkNodes

--- BTree/Walk.hs
+++ BTree/Walk.hs
@@ -1,8 +1,23 @@
-module BTree.Walk (walk) where
+module BTree.Walk ( walk
+                  , walkNodes
+                  ) where
 
 import BTree.Types
 import qualified Data.ByteString.Lazy as LBS
 import Pipes
+import Data.Binary
+import Data.Binary.Get (runGetOrFail)
 
-walk :: Pipe LBS.ByteString (k,v) m ()
+walk :: Monad m => Pipe LBS.ByteString (k,v) m ()
 walk = undefined
+
+walkNodes :: (Binary k, Monad m)
+          => LBS.ByteString -> Producer (BTree k OnDisk v) m (LBS.ByteString, Maybe String)
+walkNodes = go
+  where go bs = case runGetOrFail get bs of
+                  Left (rest,_,err)  -> return (rest, Just err)
+                  Right (rest,_,a)   -> do
+                    yield a
+                    if LBS.null rest
+                      then return (rest, Nothing)
+                      else go rest
