5af6a489a5e0305a67c318034dfcf0a853bf4273
Ben Gamari <bgamari.foss@gmail.com>
2013-08-26 09:32:48 -0400

    Merge: Implement more building blocks

--- BTree/Merge.hs
+++ BTree/Merge.hs
@@ -2,6 +2,10 @@
 
 module BTree.Merge (merge) where
 
+import Control.Applicative
+import Data.Function (on)
+import Data.List (sortBy)
+import Data.Either (rights)
 import Data.Maybe (fromMaybe)
 import Data.Foldable
 import Control.Monad.State hiding (forM_)
@@ -16,14 +20,25 @@ import Pipes
 import BTree.Types
 import BTree.Builder
 
-merge :: (Monad m, Functor m)
-      => (a -> a -> Ordering) -> [Producer a m ()] -> Producer a m ()
-merge compare producers = do
+mergeStreams :: (Monad m, Functor m)
+             => (a -> a -> Ordering) -> [Producer a m ()] -> Producer a m ()
+mergeStreams compare producers = do
     xs <- lift $ rights <$> mapM Pipes.next producers
     go xs
-  where go :: (Monad m, Functor m, Ord a) => [(a, Producer a m ())] -> Producer a m ()
+  where --go :: (Monad m, Functor m) => [(a, Producer a m ())] -> Producer a m ()
         go [] = return ()
         go xs = do let (a,producer):xs' = sortBy (compare `on` fst) xs
                    yield a
                    x' <- lift $ next producer
                    go $ either (const xs') (:xs') x'
+
+combine :: Monad m => (a -> a -> Bool) -> (a -> a -> a) -> Pipe a a m r
+combine eq append = await >>= go
+  where go a = do
+          a' <- await
+          if a `eq` a'
+            then go (a `append` a')
+            else yield a >> go a'
+    
+merge :: [Producer (BTree k OnDisk v) m r] -> Producer (BTree k OnDisk v) m r
+merge = undefined
