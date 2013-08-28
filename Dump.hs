1a3077abe3546ae1d8a7fcaac17fd504add9dfb8
Ben Gamari <bgamari.foss@gmail.com>
2013-08-27 21:22:26 -0400

    Dump: Take filename from command line

--- Dump.hs
+++ Dump.hs
@@ -4,9 +4,11 @@ import BTree.Walk
 import BTree.Types
 import Data.Binary
 import Data.Binary.Get
+import System.Environment
        
 main = do
-    r <- LBS.readFile "hello.btree"
+    fname:_ <- getArgs
+    r <- LBS.readFile fname
     let Right (rest,_,hdr) = runGetOrFail get r
     print (hdr :: BTreeHeader Int Int)
     result <- run $ for (walkNodesWithOffset rest)
