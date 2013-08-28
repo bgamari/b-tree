5c3b3a6669774628f56ab525799a31772ee5f4b1
Ben Gamari <bgamari.foss@gmail.com>
2013-08-27 21:23:01 -0400

    Test: Test merging

--- Test.hs
+++ Test.hs
@@ -9,11 +9,17 @@ import Data.Int
 
 n = 16
 things :: [BLeaf Int64 Int64]
-things = [BLeaf (i) (0x1000*i) | i <- [0..n-1]]
+things = [BLeaf (i) (1000*i) | i <- [0..n-1]]
+
+open' = L.open :: FilePath -> IO (Either String (LookupTree Int64 Int64))
 
 main = do
-    fromOrderedToFile 4 (fromIntegral n) "hello.btree" (each things)
-
-    Right lt <- L.open "hello.btree"
-             :: IO (Either String (LookupTree Int64 Int64))
+    fromOrderedToFile 4 (fromIntegral n) "hello1.btree" (each things)
+    fromOrderedToFile 4 (fromIntegral n) "hello2.btree" (each things)
+    
+    Right a <- open' "hello1.btree"
+    Right b <- open' "hello2.btree"
+    mergeTrees compare (+) 4 "hello-merged.btree" [a,b]
+    
+    Right lt <- open' "hello-merged.btree"
     mapM_ (print . L.lookup lt) [0..n]
