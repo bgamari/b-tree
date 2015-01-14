e818fa4e04561716cfd0b0785f61137ad7a31fcd
Ben Gamari <bgamari.foss@gmail.com>
2015-01-14 15:20:37 -0500

    TestUnorderedMerge: Reduce item count

--- tests/TestUnorderedMerge.hs
+++ tests/TestUnorderedMerge.hs
@@ -6,7 +6,7 @@ import System.Random
  
 main = runEitherT $ do
     let keys :: [Int]
-        keys = take (10*1000*1000) $ randoms $ mkStdGen 123
+        keys = take (1*1000*1000) $ randoms $ mkStdGen 123
     let prod = each $ map (\k->BLeaf k k) keys
     --let prod = each [BLeaf (i*137 `mod` 653) i | i <- [0..10000000::Int]]
     BT.fromUnorderedToFile "/tmp" 1000000 10 "out.btree" prod
