97177fa6fe52afb4ec1d58f6fbe90eaaa55e6ff2
Ben Gamari <ben@smart-cactus.org>
2016-05-19 23:10:46 +0200

    Add QuickCheck test for walk

--- tests/QuickCheck.hs
+++ tests/QuickCheck.hs
@@ -1,14 +1,18 @@
 import Test.QuickCheck
 import qualified Data.Map as M
-import Data.Foldable (foldl')
+import Data.Foldable (foldl', foldMap)
 import Data.Binary (Binary)
 import Pipes hiding (discard)
+import qualified Pipes.Prelude as PP
 import qualified BTree as BT
 
 mapToBLeafs :: M.Map k v -> [BT.BLeaf k v]
 mapToBLeafs = map (\(k,v)->BT.BLeaf k v) . M.toAscList
 
--- | Test correctness of exact-sized input
+mapFromBLeafs :: Ord k => [BT.BLeaf k v] -> M.Map k v
+mapFromBLeafs = foldMap (\(BT.BLeaf k v) -> M.singleton k v)
+
+-- | Test lookup correctness of exact-sized input
 testExact :: (Ord k, Eq v, Binary k, Binary v)
           => M.Map k v -> Property
 testExact m = ioProperty $ do
@@ -17,7 +21,7 @@ testExact m = ioProperty $ do
     let Right bt = BT.fromByteString bs
     return $ all (\(k,v)->BT.lookup bt k == Just v) (M.assocs m)
 
--- | Test correctness of inexact-sized input
+-- | Test lookup correctness of inexact-sized input
 test :: (Ord k, Eq v, Binary k, Binary v)
      => BT.Size -> M.Map k v -> Property
 test size m = ioProperty $ do
@@ -25,6 +29,15 @@ test size m = ioProperty $ do
     let Right bt = BT.fromByteString bs
     return $ all (\(k,v)->BT.lookup bt k == Just v) (take (fromIntegral size) $ M.toAscList m)
 
+testWalk :: (Ord k, Eq v, Binary k, Binary v)
+         => BT.Size -> M.Map k v -> Property
+testWalk size m = ioProperty $ do
+    bs <- BT.fromOrderedToByteString 10 size (each $ mapToBLeafs m)
+    let Right bt = BT.fromByteString bs
+        xs = map (\(BT.BLeaf k v) -> (k, v)) $ PP.toList $ void $ BT.walkLeaves bt
+    return $ xs == take (fromIntegral size) (M.toAscList m)
+
 main = do
     quickCheck (test :: BT.Size -> M.Map Int Int -> Property)
     quickCheck (testExact :: M.Map Int Int -> Property)
+    quickCheck (testWalk :: BT.Size -> M.Map Int Int -> Property)
