3aa6cb9ccdfb065a9f9f9177b70f0b0363d6aef1
Ben Gamari <ben@smart-cactus.org>
2016-06-20 18:55:55 +0200

    Kill unused language pragma

--- src/BTree/Merge.hs
+++ src/BTree/Merge.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TemplateHaskell, BangPatterns, GeneralizedNewtypeDeriving #-}
-
 module BTree.Merge ( mergeTrees
                    , mergeLeaves
                    , sizedProducerForTree
